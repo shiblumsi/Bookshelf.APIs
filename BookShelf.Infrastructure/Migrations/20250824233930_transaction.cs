9d90abaed156663c25770a0a9345cf5840e0a788
shiblu-msi <shiblu.msi@gmail.com>
2025-08-25 06:09:35 +0600

    working on SubscriptionPlan

---
+++ BookShelf.Infrastructure/Migrations/20250824233930_transaction.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class transaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
